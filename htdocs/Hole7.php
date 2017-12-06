0d9c7b42d753f32a6e73d29273b3b6a5a1abc18d
Josh Brooks <jdbb7d@mail.missouri.edu>
2017-12-06 15:14:51 -0600

    Updated info

--- htdocs/Hole7.php
+++ htdocs/Hole7.php
@@ -3,37 +3,37 @@
 	    <article>
 			<p style="display:flex;justify-content:center;">
 				
-				<img src="img/Golf Images/hole1.jpg"> <!--HOLE IMAGE GOES HERE-->
+				<img src="img/Golf Images/hole7.jpg"> <!--HOLE IMAGE GOES HERE-->
 				<table style="width: 25%; margin-left: 10%;">
 					<tbody>
-						<th align="center" valign="top" colspan="2">Hole 1</th>
+						<th align="center" valign="top" colspan="2">Hole 7</th>
 						<tr>
 							<td>Par</td>
-							<td>4</td>
+							<td>3</td>
 						</tr>
 						<tr>
 							<td>Tournament</td>
-							<td>422</td>
+							<td>258</td>
 						</tr>
 						<tr>
 							<td>Back</td>
-							<td>403</td>
+							<td>220</td>
 						</tr>
 						<tr>
 							<td>Middle</td>
-							<td>378</td>
+							<td>190</td>
 						</tr>
 						<tr>
 							<td>Intermediate</td>
-							<td>353</td>
+							<td>148</td>
 						</tr>
 						<tr>
 							<td>Forward</td>
-							<td>321</td>
+							<td>117</td>
 						</tr>
 					</tbody>
 				</table>
 			</p>
-	      	<p>The opening hole at Old Hawthorne embodies many of the attributes of the other holes throughout the golf course, a beautiful natural setting, a wide playing corridor and a variety of strategic options.  The mid-length par 4 plays from atop a large bluff, sharply downhill from the tees to a green nestled up to Grindstone Creek.  Players who are distracted by the beauty of the surroundings run the risk of missing the subtleties of the strategy here.  The wide, bunkerless fairway will require some attention if players want to set up their approach shot to the green.  The crowned fairway will direct shots left or right depending upon where the tee shot lands.  The hole location and the deep greenside bunker will help influence which part of the fairway players will want to play to.</p>
+	      	<p>Coming off the longest hole on the golf course, golfers will now encounter the longest par 3 on the course.  The 7th hole plays to a length of 258 yards from the tournament tees and will require everyone to hit their best tee shot here if they hope to make par.  There are no bunkers on this hole and no hazards are in play either, but the hole will still require the most out of every player.  The large green is set into a slight hillside which creates some support on the right and back side of the green.  Some slight undulations within the green help to define a variety of hole locations and there is plenty of fairway front and left of the green to allow players to run a shot up onto the green.</p>
 
 	    </article>
