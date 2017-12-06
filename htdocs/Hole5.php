8fc1d0f952335c9654ca5f70449451f6d290bab2
Josh Brooks <jdbb7d@mail.missouri.edu>
2017-12-06 15:11:57 -0600

    Updated info

--- htdocs/Hole5.php
+++ htdocs/Hole5.php
@@ -3,37 +3,37 @@
 	    <article>
 			<p style="display:flex;justify-content:center;">
 				
-				<img src="img/Golf Images/hole1.jpg"> <!--HOLE IMAGE GOES HERE-->
+				<img src="img/Golf Images/hole5.jpg"> <!--HOLE IMAGE GOES HERE-->
 				<table style="width: 25%; margin-left: 10%;">
 					<tbody>
-						<th align="center" valign="top" colspan="2">Hole 1</th>
+						<th align="center" valign="top" colspan="2">Hole 5</th>
 						<tr>
 							<td>Par</td>
 							<td>4</td>
 						</tr>
 						<tr>
 							<td>Tournament</td>
-							<td>422</td>
+							<td>393</td>
 						</tr>
 						<tr>
 							<td>Back</td>
-							<td>403</td>
+							<td>361</td>
 						</tr>
 						<tr>
 							<td>Middle</td>
-							<td>378</td>
+							<td>326</td>
 						</tr>
 						<tr>
 							<td>Intermediate</td>
-							<td>353</td>
+							<td>300</td>
 						</tr>
 						<tr>
 							<td>Forward</td>
-							<td>321</td>
+							<td>254</td>
 						</tr>
 					</tbody>
 				</table>
 			</p>
-	      	<p>The opening hole at Old Hawthorne embodies many of the attributes of the other holes throughout the golf course, a beautiful natural setting, a wide playing corridor and a variety of strategic options.  The mid-length par 4 plays from atop a large bluff, sharply downhill from the tees to a green nestled up to Grindstone Creek.  Players who are distracted by the beauty of the surroundings run the risk of missing the subtleties of the strategy here.  The wide, bunkerless fairway will require some attention if players want to set up their approach shot to the green.  The crowned fairway will direct shots left or right depending upon where the tee shot lands.  The hole location and the deep greenside bunker will help influence which part of the fairway players will want to play to.</p>
+	      	<p>Upon leaving the fourth green, the players will come up a slight hill and through a strand of trees  opening up onto the fifth tee.  From here they will get their first look at the hole.  It is a short risk/reward par 4 that doglegs left around a large lake on the left-hand side of the hole.  The hole sits beautifully within a natural valley.  Two bunkers located out in the landing area create a variety of options for the player on this cape style hole.  Down the center line of the hole, it plays to a yardage of 393 yards from the tournament tees, but the strong dogleg actually gives players an opportunity to go for the green in one if they feel that they can carry the entire length of the pond with their tee shot.  For everyone else there are still two or three different ways in which the hole can be successfully navigated.  An aggressive play off the tee will bring the pot bunker in the middle of the fairway into play.  A more conservative play off the tee to the wide part of the fairway will leave a longer approach shot into the green, though still only a mid to short iron. The green surface itself is rather large and undulating with a strong ridge running into the center of the green from the back side, splitting the green into two halves.  The hole location on any given day will help dictate the strategy that players find most successful on this hole.</p>
 
 	    </article>
