85d5067cf8f1a2da4b845c8fb25b1b03cf62ec42
Josh Brooks <jdbb7d@mail.missouri.edu>
2017-12-06 15:13:31 -0600

    Updated info

--- htdocs/Hole6.php
+++ htdocs/Hole6.php
@@ -3,37 +3,37 @@
 	    <article>
 			<p style="display:flex;justify-content:center;">
 				
-				<img src="img/Golf Images/hole1.jpg"> <!--HOLE IMAGE GOES HERE-->
+				<img src="img/Golf Images/hole6.jpg"> <!--HOLE IMAGE GOES HERE-->
 				<table style="width: 25%; margin-left: 10%;">
 					<tbody>
-						<th align="center" valign="top" colspan="2">Hole 1</th>
+						<th align="center" valign="top" colspan="2">Hole 6</th>
 						<tr>
 							<td>Par</td>
-							<td>4</td>
+							<td>5</td>
 						</tr>
 						<tr>
 							<td>Tournament</td>
-							<td>422</td>
+							<td>662</td>
 						</tr>
 						<tr>
 							<td>Back</td>
-							<td>403</td>
+							<td>614</td>
 						</tr>
 						<tr>
 							<td>Middle</td>
-							<td>378</td>
+							<td>564</td>
 						</tr>
 						<tr>
 							<td>Intermediate</td>
-							<td>353</td>
+							<td>524</td>
 						</tr>
 						<tr>
 							<td>Forward</td>
-							<td>321</td>
+							<td>479</td>
 						</tr>
 					</tbody>
 				</table>
 			</p>
-	      	<p>The opening hole at Old Hawthorne embodies many of the attributes of the other holes throughout the golf course, a beautiful natural setting, a wide playing corridor and a variety of strategic options.  The mid-length par 4 plays from atop a large bluff, sharply downhill from the tees to a green nestled up to Grindstone Creek.  Players who are distracted by the beauty of the surroundings run the risk of missing the subtleties of the strategy here.  The wide, bunkerless fairway will require some attention if players want to set up their approach shot to the green.  The crowned fairway will direct shots left or right depending upon where the tee shot lands.  The hole location and the deep greenside bunker will help influence which part of the fairway players will want to play to.</p>
+	      	<p>On a golf course with many gorgeous settings, the sixth hole could be the prettiest of all.  This is the longest hole on the golf course playing to a par 5 and a length of 662 yards from the tournament tees.  The tees sit on a high ridge with the player looking down through the first landing area, across a valley and up in to the second landing area set into another ridge that helps turn the hole softly from right to left.  The green rests next to a small pond.  It's a wonderful setting for a very challenging golf hole.  Players are given the opportunity to bomb away off the tee box as there is no real trouble in the landing area except for the longest hitters, who might need to keep in mind the creek that bisects the fairway.  From the first landing area the golf hole really starts to get interesting.  The longer the player hits it off the tee, the tougher the decision gets on the second shot, as the bunkers that are set in to the hillside on the left-hand side of the landing area come in to play.  The small pot bunker in the middle of the fairway at about the 537 yard mark off the tournament tee will be a key feature for players to consider as they decide whether to lay up, go over it trying to get as close to the green as possible, or play to either side of it.  The green is relatively small, but also relatively flat with very little contour within the surface.  The green sits hard against the ponds' edge on the left-hand side with some fairway bailout available on the right-hand side of the green.  The 6th hole is a true three shot par 5 that will require players to plan their shots and execute their plan if they are to be successful.</p>
 
 	    </article>
