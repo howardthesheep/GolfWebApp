e9a31786dad00408cabe2d70a8bd3751adda200f
Josh Brooks <jdbb7d@mail.missouri.edu>
2017-12-06 15:25:32 -0600

    Updated info

--- htdocs/Hole14.php
+++ htdocs/Hole14.php
@@ -3,37 +3,37 @@
 	    <article>
 			<p style="display:flex;justify-content:center;">
 				
-				<img src="img/Golf Images/hole1.jpg"> <!--HOLE IMAGE GOES HERE-->
+				<img src="img/Golf Images/hole14.jpg"> <!--HOLE IMAGE GOES HERE-->
 				<table style="width: 25%; margin-left: 10%;">
 					<tbody>
-						<th align="center" valign="top" colspan="2">Hole 1</th>
+						<th align="center" valign="top" colspan="2">Hole 14</th>
 						<tr>
 							<td>Par</td>
 							<td>4</td>
 						</tr>
 						<tr>
 							<td>Tournament</td>
-							<td>422</td>
+							<td>431</td>
 						</tr>
 						<tr>
 							<td>Back</td>
-							<td>403</td>
+							<td>411</td>
 						</tr>
 						<tr>
 							<td>Middle</td>
-							<td>378</td>
+							<td>380</td>
 						</tr>
 						<tr>
 							<td>Intermediate</td>
-							<td>353</td>
+							<td>341</td>
 						</tr>
 						<tr>
 							<td>Forward</td>
-							<td>321</td>
+							<td>310</td>
 						</tr>
 					</tbody>
 				</table>
 			</p>
-	      	<p>The opening hole at Old Hawthorne embodies many of the attributes of the other holes throughout the golf course, a beautiful natural setting, a wide playing corridor and a variety of strategic options.  The mid-length par 4 plays from atop a large bluff, sharply downhill from the tees to a green nestled up to Grindstone Creek.  Players who are distracted by the beauty of the surroundings run the risk of missing the subtleties of the strategy here.  The wide, bunkerless fairway will require some attention if players want to set up their approach shot to the green.  The crowned fairway will direct shots left or right depending upon where the tee shot lands.  The hole location and the deep greenside bunker will help influence which part of the fairway players will want to play to.</p>
+	      	<p>The 14th hole is a mid-length par 4 doglegging sharply right.  Three small bunkers are set into a natural ridge running through the landing area, creating two distinct options for players off the tee.  Playing into the lower right-hand fairway is risky, but shortens the hole substantially.  The upper left-hand fairway is large and accommodating, but the angle of approach will bring the greenside bunker into play, while also lengthening the hole.  As is the theme throughout the golf course, players are given options of play throughout.</p>
 
 	    </article>
