51154f181c9b5dc40e32bb0da52eb1055325e3d0
Josh Brooks <jdbb7d@mail.missouri.edu>
2017-12-06 15:20:05 -0600

    Updated info

--- htdocs/Hole10.php
+++ htdocs/Hole10.php
@@ -3,37 +3,37 @@
 	    <article>
 			<p style="display:flex;justify-content:center;">
 				
-				<img src="img/Golf Images/hole1.jpg"> <!--HOLE IMAGE GOES HERE-->
+				<img src="img/Golf Images/hole10.jpg"> <!--HOLE IMAGE GOES HERE-->
 				<table style="width: 25%; margin-left: 10%;">
 					<tbody>
-						<th align="center" valign="top" colspan="2">Hole 1</th>
+						<th align="center" valign="top" colspan="2">Hole 10</th>
 						<tr>
 							<td>Par</td>
-							<td>4</td>
+							<td>5</td>
 						</tr>
 						<tr>
 							<td>Tournament</td>
-							<td>422</td>
+							<td>530</td>
 						</tr>
 						<tr>
 							<td>Back</td>
-							<td>403</td>
+							<td>510</td>
 						</tr>
 						<tr>
 							<td>Middle</td>
-							<td>378</td>
+							<td>486</td>
 						</tr>
 						<tr>
 							<td>Intermediate</td>
-							<td>353</td>
+							<td>435</td>
 						</tr>
 						<tr>
 							<td>Forward</td>
-							<td>321</td>
+							<td>339</td>
 						</tr>
 					</tbody>
 				</table>
 			</p>
-	      	<p>The opening hole at Old Hawthorne embodies many of the attributes of the other holes throughout the golf course, a beautiful natural setting, a wide playing corridor and a variety of strategic options.  The mid-length par 4 plays from atop a large bluff, sharply downhill from the tees to a green nestled up to Grindstone Creek.  Players who are distracted by the beauty of the surroundings run the risk of missing the subtleties of the strategy here.  The wide, bunkerless fairway will require some attention if players want to set up their approach shot to the green.  The crowned fairway will direct shots left or right depending upon where the tee shot lands.  The hole location and the deep greenside bunker will help influence which part of the fairway players will want to play to.</p>
+	      	<p>The back nine at Old Hawthorne begins with a relatively short but very challenging par 5.  The tee shot on this hole could be the most intimidating shot on the golf course.  It plays from an elevated tee down to a fairway that slopes hard from left to right, with the entire right-hand side of the fairway through the first landing area guarded by a large lake.  A well played tee shot can carry the ridge running across the fairway, giving the golfers a little added distance as their shot runs down the slope.  Once in the landing area they will encounter a number of options that they need to consider for their second shot.  The green sits on the far side of Grindstone Creek and there is fairway on both side of the creek.  A large tree also divides the fairway on the far side and some greenside and fairway bunkering further defines the players options.  The green is reachable in two but the orientation of the green and the contours within the surface will require a player to hit a precise second shot if they expect to have any hope of eagle or birdie.  Players not playing for the green in two still have three or four options with which to choose from in deciding whether or not to challenge the creek on their second shot or play across the creek with a short third shot to the well-protected green.  Like most of the holes on this course, hole 10 will require golfers to match their chosen strategy to their ability to execute that strategy.</p>
 
 	    </article>
