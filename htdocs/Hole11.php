5cdad76e5e2254d495e6977fda6f1944fcfac2ea
Josh Brooks <jdbb7d@mail.missouri.edu>
2017-12-06 15:21:07 -0600

    Updated info

--- htdocs/Hole11.php
+++ htdocs/Hole11.php
@@ -3,37 +3,37 @@
 	    <article>
 			<p style="display:flex;justify-content:center;">
 				
-				<img src="img/Golf Images/hole1.jpg"> <!--HOLE IMAGE GOES HERE-->
+				<img src="img/Golf Images/hole11.jpg"> <!--HOLE IMAGE GOES HERE-->
 				<table style="width: 25%; margin-left: 10%;">
 					<tbody>
-						<th align="center" valign="top" colspan="2">Hole 1</th>
+						<th align="center" valign="top" colspan="2">Hole 11</th>
 						<tr>
 							<td>Par</td>
 							<td>4</td>
 						</tr>
 						<tr>
 							<td>Tournament</td>
-							<td>422</td>
+							<td>467</td>
 						</tr>
 						<tr>
 							<td>Back</td>
-							<td>403</td>
+							<td>425</td>
 						</tr>
 						<tr>
 							<td>Middle</td>
-							<td>378</td>
+							<td>395</td>
 						</tr>
 						<tr>
 							<td>Intermediate</td>
-							<td>353</td>
+							<td>369</td>
 						</tr>
 						<tr>
 							<td>Forward</td>
-							<td>321</td>
+							<td>301</td>
 						</tr>
 					</tbody>
 				</table>
 			</p>
-	      	<p>The opening hole at Old Hawthorne embodies many of the attributes of the other holes throughout the golf course, a beautiful natural setting, a wide playing corridor and a variety of strategic options.  The mid-length par 4 plays from atop a large bluff, sharply downhill from the tees to a green nestled up to Grindstone Creek.  Players who are distracted by the beauty of the surroundings run the risk of missing the subtleties of the strategy here.  The wide, bunkerless fairway will require some attention if players want to set up their approach shot to the green.  The crowned fairway will direct shots left or right depending upon where the tee shot lands.  The hole location and the deep greenside bunker will help influence which part of the fairway players will want to play to.</p>
+	      	<p>The 11th hole is a long par 4 that will require two well played shots for anyone hoping to make par.   The tee shot plays uphill to one of the widest landing areas on the golf course.  The landing area is deceptive though, as the view and angle of approach to the green is much better from one side of the landing area than the other.  The landing area is bisected by one pot bunker, creating a left and right option for the majority of players.  The right-hand fairway is slightly elevated, requiring a stronger tee shot.  The reward for finding this part of the fairway is a full view of the green and a good angle of approach.  The green is fronted by a single bunker and sits atop a small knob amongst a stand of trees.</p>
 
 	    </article>
