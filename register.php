41c6b37e80a53a134b5930a6e94f1651386e74e4
Josh Brooks <jdbb7d@mail.missouri.edu>
2017-12-07 16:00:43 -0600

    Fixed user form

--- register.php
+++ register.php
@@ -7,8 +7,10 @@
         $conn = new mysqli($servername, $username, $password, $dbname);
         $myusername = $_POST['user'];
         $mypassword = $_POST['pass'];
+        $firstName = $_POST['fname'];
+        $lastName = $_POST['lname'];
 //    hash('sha512',$mypassword);
-        $sql = "INSERT INTO users (username, password) VALUES ('$myusername', '$mypassword')";
+        $sql = "INSERT INTO users (username, password, fname, lname) VALUES ('$myusername', '$mypassword', '$firstName', '$lastName')";
         $result = mysqli_query($conn,$sql);
         if($result){ 
             echo "Account successfully created!";
@@ -29,7 +31,12 @@
             <div id="registerForm">
                 <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off">
                     <h2>Register</h2>
+                    <input type="text" name="fname" value placeholder="First" maxlength="255" size="25">
+                    <br>
+                    <input type="text" name="lname" value placeholder="Last" maxlength="255" size="25">
+                    <br>
                     <input type="text" name="user" value placeholder="Username" maxlength="255" size="25">
+                    <br>
                     <input type="password" name="pass" value placeholder="Password" maxlength="255" size="25">
                     <br>
                     <input type="submit" name="submit" value="Create account">
