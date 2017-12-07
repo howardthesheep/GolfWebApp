f4fdc1fd34bfb54b7cc48a2a7c4c2e05057b8a13
Josh Brooks <jdbb7d@mail.missouri.edu>
2017-12-07 17:36:09 -0600

    Updated info

--- htdocs/about.php
+++ htdocs/about.php
@@ -2,7 +2,14 @@
 	 define("PAGE_TITLE", 'About');
 	 include("template/header.php");?>
 
-	 <!-- HTML goes here -->
-	 
-
+<!DOCTYPE html>
+<html>
+    <body>
+        <section class="content" style="display: block;">
+            <p style="display:flex;justify-content:center;">The Club at Old Hawthorne is a private country club nestled within the Community of Old Hawthorne in the rolling hills a few minutes east of downtown Columbia, Missouri.  Established in 2007, The Club at Old Hawthorne provides its' members with a memorable private club experience every time they visit.</p>
+            
+            <p style="display:flex;justify-content:center;">This website was created and designed by Josh Brooks and Parker Jones.</p>
+        </section>
+    </body>
+</html>
 <?php include("template/footer.php");?>
